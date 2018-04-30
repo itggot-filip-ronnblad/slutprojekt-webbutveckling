e6e331666ea46e7ec2f0d2f55f8513d29f17f80d
itggot-filip-ronnblad <filip.ronnblad@itggot.se>
2018-04-30 15:56:44 +0200

    Försökte fixa menyn. Lyckades inte

--- slutprojekt-webbutveckling/site/js/nav.js
+++ slutprojekt-webbutveckling/site/js/nav.js
@@ -1,8 +1,7 @@
-function myFunction(){
-    var x = document.getElementsByClassName("side_nav");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else{
-        x.style.display = "block";
-    }
+function openNav(){
+    document.getElementsByClassName("side_nav").style.width = "150px";
+}
+
+function closeNav(){
+    document.getElementsByClassName("side_nav").style.width = "0px"
 }
